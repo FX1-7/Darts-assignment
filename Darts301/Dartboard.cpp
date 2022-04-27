2bf0dc85c9780ee274572cef23c3f24d7c814065
Keiranos <31273773+Keiranos@users.noreply.github.com>
2022-04-27 23:43:37 +0100

    Fix to bullseye targetting

--- Darts301/Dartboard.cpp
+++ Darts301/Dartboard.cpp
@@ -8,18 +8,14 @@ int Dartboard::Throw(int p) {
 	// Board neighbours ignoring slot zero
 	int bd[2][21] = { {0,20,15,17,18,12,13,19,16,14,6,8,9,4,11,10,7,2,1,3,5},
 			   {0,18,17,19,13,20,10,16,11,12,15,14,5,6,9,2,8,3,4,7,1} };
+	int bdwb[22] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,50 };
 	int r = rand() % 100 + 1;
 	int h = rand() % 20 + 1;
 
 	if (h == 20) {
-		int cf = rand() % 2;
-		if (cf == 1) {
-			return 50;
+		int cf = rand() % 20 + 1;
+		return bdwb[cf];
 		}
-		else {
-			return 20;
-		}
-	}
 	else {
 		if (r <= p) {
 			return h;
